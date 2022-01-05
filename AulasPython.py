5f0be4e44b578266eee329c1ba5926c12d76e301
tiago <tiagoribeirox@hotmail.com>
2022-01-04 21:23:35 -0300

    Novo Python

---
+++ AulasPython.py
@@ -0,0 +1,53 @@
+#nome = 'Tiago'
+#idade  = str(33)
+#print ('Meu nome é ' + nome + ' e tenho ' + idade + ' anos de idade')
+
+#nome = 'tiago'
+#sobrenome = 'ribeiro'
+#profissao = 'DEV'
+#texto = f'O {nome} {sobrenome} é um ótimo {profissao}'
+#print(texto)
+
+#nome = input('Qual o seu nome? ')
+#print ('O nome digitado é '  + nome + '.')
+
+#ano_nascimento = input('Em que ano voce nasceu?')
+#idade = 2021 - int(ano_nascimento)
+#print (idade)
+
+#mensagem = 'Eu adoro comida Caseira' 
+#print (mensagem.upper()) #DEIXA LETRAS TUDO EM MAIUSCULAS
+
+#mensagem = 'eu adoro comida Caseira' 
+#print (mensagem.capitalize()) #DEIXA A PRIMEIRA LETRA MAIUSCULA
+
+#mensagem = 'eu adoro comida Caseira' 
+#print (mensagem.find('c')) #PROCURA LETRA OU PALAVRA NA POSIÇÃO DO INDEX (0123456789...)
+
+#mensagem = 'eu adoro comida Caseira' 
+#print (mensagem.replace('a', 'e')) #ALTERA LETRAS 'A' PELA 'E'
+
+#mensagem = 'eu adoro comida Caseira' 
+#print (mensagem.replace('Caseira', 'feito em casa')) #ALTERA PALAVRAS
+
+#mensagem = '     eu adoro comida Caseira' 
+#print (mensagem.strip()) #REMOVE ESPAÇOS ANTES DO PRIMEIRO CARACTERE
+
+#numero = 50
+#chute = int(input('Digite um numero: '))
+#if chute == numero:
+#    print('Você acertou!')
+#else: 
+#    print('Você errou!')    
+
+#nome = input('Digite um nome: ')
+#if nome == 'Tiago':
+#    print('Você acertou!')
+#else:
+#    print('Você errou')
+
+#data_atual = int(input('Digite a data atual: '))
+#ano_nascimento = int(input('Digite o ano nascimento: '))
+#idade = data_atual - ano_nascimento
+#print(f'Sua idade atual é {idade} anos')
+
