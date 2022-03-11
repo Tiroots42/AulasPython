ffa046fdb8cbb869d2866ee91c19f9684e4f2f42
tiago <tiagoribeirox@hotmail.com>
2022-03-11 20:20:03 -0300

    Python

--- AulasPython.py
+++ AulasPython.py
@@ -72,3 +72,17 @@
 #elif(abaixo):
 #    print('Está abaixo')
 
+#renda_acima_5mil = True
+#nome_limpo = True
+#if renda_acima_5mil and nome_limpo:
+#    print("Aprovado")
+#else:
+#    print("Reprovado")
+
+#renda_acima_5mil = True
+#nome_limpo = True
+#if renda_acima_5mil or nome_limpo:
+#    print("Aprovado")
+#else:
+#    print("Reprovado")
+
